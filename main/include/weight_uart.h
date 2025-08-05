c9f2282c9bbb72d48ef9269931a691a53ea2c070
deleskap <80005731+deleskap@users.noreply.github.com>
2025-08-05 17:28:42 +0200

    Add RS232 weight reading via UART

---
+++ main/include/weight_uart.h
@@ -0,0 +1,18 @@
+#ifndef WEIGHT_UART_H
+#define WEIGHT_UART_H
+
+#include "common.h"
+#include "driver/uart.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+esp_err_t weight_uart_init(void);
+bool weight_uart_read(float *weight);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // WEIGHT_UART_H
